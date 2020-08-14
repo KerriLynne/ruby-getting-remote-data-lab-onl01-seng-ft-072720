fccceca95f0093a9fee2dd620193403e804448c9
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-14 13:28:25 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,12 +1,15 @@
 require 'open-uri'
 require 'net/http'
+require 'json'
 
 class GetRequester
 
-  URL = 
+  URL =
 
 
   def get_requester
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
   end
+
+    def get_response_body
